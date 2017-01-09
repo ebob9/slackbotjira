5612dcb16a8a4ecc840376691714486085018924
Aaron Edwards <aaron@cloudgenix.com>
2017-01-09 01:59:04 -0600

    Merged great changes from https://github.com/spitty/slackbotjira, using attachment for messages and RE to search for multiple tickes in a single line.

--- slackbotjira/slackbotjira.py
+++ slackbotjira/slackbotjira.py
@@ -1,6 +1,7 @@
 from jira import JIRA, JIRAError
 from slackbot import settings
 from slackbot.bot import respond_to, listen_to
+import re
 
 # Clean JIRA Url to not have trailing / if exists
 CLEAN_JIRA_URL = settings.JIRA_URL if not settings.JIRA_URL[-1:] == '/' else settings.JIRA_URL[:-1]
@@ -28,21 +29,25 @@ jira = JIRA( basic_auth=settings.JIRA_AUTH, options={
 
 
 # Listen for messages that look like JIRA URLs
-@listen_to('([A-Za-z]+)-([0-9]+)')
-def jira_listener(message, ticketproject, ticketnum):
+@listen_to('(.*)')
+def jira_listener(message, text):
+    for ticketproject, ticketnum in re.findall('([A-Za-z]+)-([0-9]+)', text):
+        # Only attempt to find tickets in projects defined in slackbot_settings
+        if project_case_sensitive:
+            # case sensitive searching
+            if ticketproject not in settings.JIRA_PROJECTS:
+                return
+        else:
+            # case insensitive.
+            if ticketproject.upper() not in [item.upper() for item in settings.JIRA_PROJECTS]:
+                return
 
-    # Only attempt to find tickets in projects defined in slackbot_settings
-    if project_case_sensitive:
-        # case sensitive searching
-        if ticketproject not in settings.JIRA_PROJECTS:
-            return
-    else:
-        # case insensitive.
-        if ticketproject.upper() not in [item.upper() for item in settings.JIRA_PROJECTS]:
-            return
+        # Parse ticket and search JIRA
+        ticket = '%s-%s' % (ticketproject, ticketnum)
+        process_ticket(message, ticket)
 
-    # Parse ticket and search JIRA
-    ticket = '%s-%s' % (ticketproject, ticketnum)
+
+def process_ticket(message, ticket):
     try:
         issue = jira.issue(ticket)
     except JIRAError:
@@ -54,19 +59,37 @@ def jira_listener(message, ticketproject, ticketnum):
     reporter = issue.fields.reporter.displayName if issue.fields.reporter else 'Anonymous'
     assignee = issue.fields.assignee.displayName if issue.fields.assignee else 'Unassigned'
     status = issue.fields.status
+    priority = issue.fields.priority
     ticket_url = CLEAN_JIRA_URL + '/browse/%s' % ticket
 
+    attachment = {
+        "title": ticket_url,
+        "pretext": "Ticket *%s*" % (ticket),
+        "text": summary,
+        "mrkdwn_in": ["text", "pretext"],
+        "fields": [
+            {
+                "title": "Priority",
+                "value": priority.name,
+                "short": True
+            },
+            {
+                "title": "Status",
+                "value": status.name,
+                "short": True
+            },
+            {
+                "title": "Reporter",
+                "value": reporter,
+                "short": True
+            },
+            {
+                "title": "Assignee",
+                "value": assignee,
+                "short": True
+            }
+        ]
+    }
+
     # Send message to Slack
-    message.send('''%s:
-    Summary: %s
-    Reporter: %s
-    Assignee: %s
-    Status: %s
-    ''' % (
-        ticket_url,
-        summary,
-        reporter,
-        assignee,
-        status
-    )
-                 )
+    message.send_webapi('', attachments=[attachment])
